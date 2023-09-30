71e663474519b35c45446cfba6ea999c61d8683c
Juanmaanghilante <curne_juan@hotmail.com>
2023-09-30 11:43:34 -0300

    creando estructura database

---
+++ drawSQL-mysql-export-2023-09-30.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE SEGUIMIENTO;
+USE SEGUIMIENTO;
+CREATE TABLE `gestores`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nombre` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `clientes` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `cliente`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nombre` VARCHAR(255) NOT NULL,
+    `cuit` INT UNSIGNED NOT NULL,
+    `direccion` VARCHAR(255) NOT NULL,
+    `localidad` VARCHAR(255) NOT NULL,
+    `provincia` VARCHAR(255) NOT NULL,
+    `contacto` INT NOT NULL,
+    `nombrecontacto` VARCHAR(255) NOT NULL,
+    `id_gestor` BIGINT UNSIGNED NOT NULL
+);
+CREATE TABLE `vehiculo`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `dominio` VARCHAR(255) NOT NULL,
+    `año` INT NOT NULL,
+    `modelo` VARCHAR(255) NOT NULL,
+    `patente` VARCHAR(255) NOT NULL,
+    `id_cliente` BIGINT UNSIGNED NOT NULL,
+    `id_concesionario` BIGINT UNSIGNED NOT NULL,
+    `id_tiket` BIGINT UNSIGNED NOT NULL
+);
+CREATE TABLE `concesionario`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nombre` VARCHAR(255) NOT NULL,
+    `provincia` VARCHAR(255) NOT NULL,
+    `cuit` INT NOT NULL,
+    `contacto` BIGINT NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `tickets`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id_concesionario` BIGINT UNSIGNED NOT NULL,
+    `numero_servicio` TINYINT UNSIGNED NOT NULL,
+    `create_at` DATETIME NOT NULL,
+    `delete_at` DATETIME NOT NULL,
+    `update_at` DATETIME NOT NULL,
+    `fecha_turno` DATETIME NOT NULL,
+    `proveedor_service` VARCHAR(255) NOT NULL,
+    `presupuesto` DOUBLE(8, 2) NOT NULL,
+    `reparaciones` VARCHAR(255) NOT NULL,
+    `estado_tiket` VARCHAR(255) NOT NULL,
+    `observaciones` VARCHAR(255) NOT NULL,
+    `factura` VARCHAR(255) NOT NULL,
+    `id_gestor` BIGINT UNSIGNED NOT NULL
+);
+ALTER TABLE
+    `vehiculo` ADD CONSTRAINT `vehiculo_id_concesionario_foreign` FOREIGN KEY(`id_concesionario`) REFERENCES `concesionario`(`id`);
+ALTER TABLE
+    `vehiculo` ADD CONSTRAINT `vehiculo_id_tiket_foreign` FOREIGN KEY(`id_tiket`) REFERENCES `tickets`(`id`);
+ALTER TABLE
+    `vehiculo` ADD CONSTRAINT `vehiculo_id_cliente_foreign` FOREIGN KEY(`id_cliente`) REFERENCES `cliente`(`id`);
+ALTER TABLE
+    `tickets` ADD CONSTRAINT `tickets_id_gestor_foreign` FOREIGN KEY(`id_gestor`) REFERENCES `gestores`(`id`);
+ALTER TABLE
+    `cliente` ADD CONSTRAINT `cliente_id_gestor_foreign` FOREIGN KEY(`id_gestor`) REFERENCES `gestores`(`id`);
